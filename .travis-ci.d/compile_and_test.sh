e143a5b78d30697910ff18ea494437a0cfafac7e
Remi Ete <remi.ete@gmail.com>
2020-09-25 10:44:08 +0200

    Fixed cmake command for travisCI

--- .travis-ci.d/compile_and_test.sh
+++ .travis-ci.d/compile_and_test.sh
@@ -5,7 +5,7 @@ source $ILCSOFT/init_ilcsoft.sh
 cd /Package
 mkdir build
 cd build
-cmake -GNinja -DSIO_BUILTIN_ZLIB=$BUILTIN_ZLIB -DSIO_EXAMPLES=ON -DCMAKE_CXX_FLAGS="-fdiagnostics-color=always" .. && \
+cmake -GNinja -C $ILCSOFT/ILCSoft.cmake -DCMAKE_CXX_FLAGS="-fdiagnostics-color=always" .. && \
 ninja  -k 0 && \
 ninja install 
 #&& \
